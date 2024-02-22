9fbbfc01e4b981f7d4bd806eb393e2c16a8dcc92
adit-ravi-2023 <adit.ravi@kellogg.northwestern.edu>
2024-02-22 01:46:48 +0000

    Part 1 ready

--- app/controllers/place_controller.rb
+++ app/controllers/place_controller.rb
@@ -6,8 +6,7 @@ class PlaceController < ApplicationController
 
   def show
     @place = Place.find_by({"id" => params["id"]})
-    @entries = Entry.all
-    # where(place_id: params["id"])
+    @entries = Entry.where({"place_id" => @place["id"]})
   end
 
   def new
