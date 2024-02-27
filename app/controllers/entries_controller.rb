482f86af03b144b4aa44079d008a530ecedbc49c
adit-ravi-2023 <adit.ravi@kellogg.northwestern.edu>
2024-02-27 18:03:26 +0000

    Final Submission, with both parts

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -8,7 +8,7 @@ class EntriesController < ApplicationController
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["posted_on"] = params["date"]
-    # @entry["place_id"] = @place["id"]
+    @entry["place_id"] = params["place_id"]
     @entry.save
     redirect_to "/place"
   end
